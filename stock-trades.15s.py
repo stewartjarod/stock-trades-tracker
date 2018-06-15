a092f72b7013c2974f593166fa42c597e323829e
Jarod Stewart <stewartjarod@gmail.com>
2018-06-14 20:33:26 -0600

    refactors and pep8 cleanup

--- stock-trades.15s.py
+++ stock-trades.15s.py
@@ -9,69 +9,82 @@
 # <bitbar.image>https://raw.githubusercontent.com/stewartjarod/stock-trades-tracker/master/screenshot.png</bitbar.image>
 # <bitbar.dependencies>python</bitbar.dependencies>
 
-import json, urllib2, os, subprocess, sys, time
+import json
+import os
+import subprocess
+import sys
+import time
+import urllib2
 
-icon = "iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAMAAADzapwJAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAPYQAAD2EBqD+naQAAActpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IlhNUCBDb3JlIDUuNC4wIj4KICAgPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4KICAgICAgPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIKICAgICAgICAgICAgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIgogICAgICAgICAgICB4bWxuczp0aWZmPSJodHRwOi8vbnMuYWRvYmUuY29tL3RpZmYvMS4wLyI+CiAgICAgICAgIDx4bXA6Q3JlYXRvclRvb2w+QWRvYmUgSW1hZ2VSZWFkeTwveG1wOkNyZWF0b3JUb29sPgogICAgICAgICA8dGlmZjpPcmllbnRhdGlvbj4xPC90aWZmOk9yaWVudGF0aW9uPgogICAgICA8L3JkZjpEZXNjcmlwdGlvbj4KICAgPC9yZGY6UkRGPgo8L3g6eG1wbWV0YT4KKS7NPQAAAW5QTFRFAAAAAAAAAAAAAACAAICAgICAVVVVQEBAMzMzMzNmK1VVM01NLkZGM0REOkJKOEBINEFIOD5LN0NJNUBKN0FGN0FLNUJKOUFJOEBIOEJJNkFHNkFLOEJJOEFIOUJIOEFLN0FKNkJINkFJOEJKN0JKN0FJNkBHNkBKOEJJN0FJNkBIOEJJOEFIOEFKNkFIN0BJNkFINkFKOEBJN0JIN0FINkFJNkBJOEBJOEBIOEJIOEJKN0FJNkJJOEFJN0JJNkFIN0JJN0FKNkFJNkBJOEBJN0FIN0FJN0FJNkBIOEFKN0FJN0FINkFJOEFJN0FIOEFKN0FKNkBJOEBJN0JJN0BJN0FKNkFJOEFJN0FJN0FJN0FJN0FJN0FJNkFJN0FJN0FJN0FJN0FJN0FJN0FJNkFJN0FJN0FJOEFJN0FJN0FJN0FJN0FJOEJKOURMOkRMOkRNOkVNOkVOO0VOO0ZOO0ZPPEdPPEdQPUhQPUhRQUxWN2zrtgAAAGx0Uk5TAAECAgICAwQFBQYKCw8fICcpKjAzMz4/QElLS01OUVJTVV5gYWJkZGVmZ2lubnFzdXV3eHl6e3t8fICChImLjZCRlpeXmJmam5yeoqSlpqqtrq6vuru8vMjLzM3P09vf5Obo7u/w8fj5+/z+f4t3XAAAAUtJREFUGBlVwWcjQmEYgOH7abxvZYvI3mTvvbJDZkbnJNnbCQn/XtEHroscwd3V5Ub4T5hLp+cQ/hEwLcsEIUfIUjTc3TWgyBJQOEC0DeJxsGkBB4oMpwZqTx4fT2oB7SSjOVIFVBymrZsbK31QCVRFmjkPhnT90fvTvi8W8+0/vR/V69D8ObGLs9PX28kSiEahZPL27fTsMkZpeDG8GyzAjmlipyC4F17aKiVjzFszhcIwUEzVeMfIcFO2CiuFYBhQtAyr5bhxMLwZ6NzpAMOAlp1Ax1Y/Cqgb7O4Z8oNhIuNrC0sr03ngpOn+OXldDYYJs6m3l48QP7x9vcWAaQKNofVWQchRcHxMjpClXC6Xw+ZPJPyiPMqphT+ilhXlL8Ezs72ReHhIbGzPeBB+2cm/+vpMJZOpz6+rPOz8EnTbxMDI6OjIwES7RoBv8fI7D+ewmsIAAAAASUVORK5CYII="
+scales_icon = "iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAMAAADzapwJAAAAAXNSR0IArs4c6QAAAAlwSFlzAAAPYQAAD2EBqD+naQAAActpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IlhNUCBDb3JlIDUuNC4wIj4KICAgPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4KICAgICAgPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIKICAgICAgICAgICAgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIgogICAgICAgICAgICB4bWxuczp0aWZmPSJodHRwOi8vbnMuYWRvYmUuY29tL3RpZmYvMS4wLyI+CiAgICAgICAgIDx4bXA6Q3JlYXRvclRvb2w+QWRvYmUgSW1hZ2VSZWFkeTwveG1wOkNyZWF0b3JUb29sPgogICAgICAgICA8dGlmZjpPcmllbnRhdGlvbj4xPC90aWZmOk9yaWVudGF0aW9uPgogICAgICA8L3JkZjpEZXNjcmlwdGlvbj4KICAgPC9yZGY6UkRGPgo8L3g6eG1wbWV0YT4KKS7NPQAAAW5QTFRFAAAAAAAAAAAAAACAAICAgICAVVVVQEBAMzMzMzNmK1VVM01NLkZGM0REOkJKOEBINEFIOD5LN0NJNUBKN0FGN0FLNUJKOUFJOEBIOEJJNkFHNkFLOEJJOEFIOUJIOEFLN0FKNkJINkFJOEJKN0JKN0FJNkBHNkBKOEJJN0FJNkBIOEJJOEFIOEFKNkFIN0BJNkFINkFKOEBJN0JIN0FINkFJNkBJOEBJOEBIOEJIOEJKN0FJNkJJOEFJN0JJNkFIN0JJN0FKNkFJNkBJOEBJN0FIN0FJN0FJNkBIOEFKN0FJN0FINkFJOEFJN0FIOEFKN0FKNkBJOEBJN0JJN0BJN0FKNkFJOEFJN0FJN0FJN0FJN0FJN0FJNkFJN0FJN0FJN0FJN0FJN0FJN0FJNkFJN0FJN0FJOEFJN0FJN0FJN0FJN0FJOEJKOURMOkRMOkRNOkVNOkVOO0VOO0ZOO0ZPPEdPPEdQPUhQPUhRQUxWN2zrtgAAAGx0Uk5TAAECAgICAwQFBQYKCw8fICcpKjAzMz4/QElLS01OUVJTVV5gYWJkZGVmZ2lubnFzdXV3eHl6e3t8fICChImLjZCRlpeXmJmam5yeoqSlpqqtrq6vuru8vMjLzM3P09vf5Obo7u/w8fj5+/z+f4t3XAAAAUtJREFUGBlVwWcjQmEYgOH7abxvZYvI3mTvvbJDZkbnJNnbCQn/XtEHroscwd3V5Ub4T5hLp+cQ/hEwLcsEIUfIUjTc3TWgyBJQOEC0DeJxsGkBB4oMpwZqTx4fT2oB7SSjOVIFVBymrZsbK31QCVRFmjkPhnT90fvTvi8W8+0/vR/V69D8ObGLs9PX28kSiEahZPL27fTsMkZpeDG8GyzAjmlipyC4F17aKiVjzFszhcIwUEzVeMfIcFO2CiuFYBhQtAyr5bhxMLwZ6NzpAMOAlp1Ax1Y/Cqgb7O4Z8oNhIuNrC0sr03ngpOn+OXldDYYJs6m3l48QP7x9vcWAaQKNofVWQchRcHxMjpClXC6Xw+ZPJPyiPMqphT+ilhXlL8Ezs72ReHhIbGzPeBB+2cm/+vpMJZOpz6+rPOz8EnTbxMDI6OjIwES7RoBv8fI7D+ewmsIAAAAASUVORK5CYII="  # NOQA
 
+# file_path = '' # TODO let user set path, suggest using dropbox...
 stocks_file = '/tmp/trade-tracker/stocks.json'
 trade_history = 'tmp/trade-tracker/trades.json'
 if not os.path.isdir('/tmp/trade-tracker'):
     os.mkdir('/tmp/trade-tracker')
 
-def displayNotification(message,title=None,soundname=None):
+
+def display_notification(message, title=None, sound_name=None):
     """
         Display an OSX notification with message title an subtitle
         sounds are located in /System/Library/Sounds or ~/Library/Sounds
     """
-    titlePart = ''
-    if(not title is None):
-        titlePart = 'with title "{0}"'.format(title)
+    title_part = ''
+    if (not title is None):
+        title_part = 'with title "{0}"'.format(title)
     # subtitlePart = ''
     # if(not subtitle is None):
     #     subtitlePart = 'subtitle "{0}"'.format(subtitle)
-    soundnamePart = ''
-    if(not soundname is None):
-        soundnamePart = 'sound name "{0}"'.format(soundname)
+    sound_name_part = ''
+    if not sound_name is None:
+        sound_name_part = 'sound name "{0}"'.format(sound_name)
     # icon = 'with icon {0}'.format('caution')
 
-    appleScriptNotification = 'display notification "{0}" {1} {2}'.format(message, titlePart, soundnamePart)
-    subprocess.call("osascript -e '{0}'".format(appleScriptNotification), shell=True)
+    apple_script_notification = 'display notification "{0}" {1} {2}'.format(message, title_part, sound_name_part)
+    subprocess.call("osascript -e '{0}'".format(apple_script_notification), shell=True)
+
 
 def get_stock_quotes(stocks):
     list = []
     for stock in stocks:
         list.append(stock['stock'])
-    response = urllib2.urlopen('https://api.iextrading.com/1.0/stock/market/batch?symbols={0}&types=quote'.format(','.join(list)))
+    response = urllib2.urlopen(
+        'https://api.iextrading.com/1.0/stock/market/batch?symbols={0}&types=quote'.format(','.join(list)))
     return json.loads(response.read())
 
-def prompt(text='', defaultAnswer='', icon='note', buttons=('Cancel','Ok'), defaultButton=1):
+
+def prompt(text='', default_answer='', icon='note', buttons=('Cancel', 'Ok'), default_button=1):
     try:
         d = locals()
         d['buttonsStr'] = ', '.join('"%s"' % button for button in buttons)
-        d['defaultButtonStr'] = isinstance(defaultButton, int) and buttons[defaultButton] or defaultButton
+        d['defaultButtonStr'] = isinstance(default_button, int) and buttons[default_button] or default_button
         return subprocess.check_output(['osascript', '-l', 'JavaScript', '-e', '''
             const app = Application.currentApplication()
             app.includeStandardAdditions = true
             const response = app.displayDialog("{text}", {{
-                defaultAnswer: "{defaultAnswer}",
+                default_answer: "{default_answer}",
                 withIcon: "{icon}",
                 buttons: [{buttonsStr}],
-                defaultButton: "{defaultButtonStr}"
+                default_button: "{defaultButtonStr}"
             }})
             response.textReturned
         '''.format(**d)]).rstrip()
     except subprocess.CalledProcessError:
         pass
 
-def entryString(title='---', **kwargs):
-    args = ' '.join('{}=\'{}\''.format(k,v) for k,v in kwargs.items() if v is not None)
+
+def entry_string(title='---', **kwargs):
+    args = ' '.join('{}=\'{}\''.format(k, v) for k, v in kwargs.items() if v is not None)
     if args: args = '|' + args
     return title + args
 
+
 def entry(title='---', **kwargs):
-    args = ' '.join('{}=\'{}\''.format(k,v) for k,v in kwargs.items() if v is not None)
+    args = ' '.join('{}=\'{}\''.format(k, v) for k, v in kwargs.items() if v is not None)
     if args: args = '|' + args
     print(title + args)
+
 
 def toggle_alert_for_ticker(ticker):
     stocks = read_stocks_file()
@@ -81,13 +94,16 @@ def toggle_alert_for_ticker(ticker):
     with open(stocks_file, 'wt') as f:
         f.write(json.dumps(stocks))
 
+
 def create_file(file, data):
     with open(file, 'wt') as f:
         f.write(data)
 
+
 def read_stocks_file():
     with open(stocks_file, 'rt') as f:
         return json.loads(f.read())
+
 
 def add_stock_to_stocks_file(stock):
     stocks = read_stocks_file()
@@ -95,6 +111,7 @@ def add_stock_to_stocks_file(stock):
     stocks[stock['stock']] = stock
     with open(stocks_file, 'wt') as f:
         f.write(json.dumps(stocks))
+
 
 def record_trade(ticker, sell_price):
     stocks = read_stocks_file()
@@ -106,11 +123,13 @@ def record_trade(ticker, sell_price):
     with open(trade_history, 'wt') as f:
         f.write(json.dumps(trades))
 
+
 def update_stock_in_stocks_file(stock):
     stocks = read_stocks_file()
     stocks[stock['stock']] = stock
     with open(stocks_file, 'wt') as f:
         f.write(json.dumps(stocks))
+
 
 def remove_stock_from_stocks_file(ticker):
     stocks = read_stocks_file()
@@ -118,16 +137,27 @@ def remove_stock_from_stocks_file(ticker):
     with open(stocks_file, 'wt') as f:
         f.write(json.dumps(stocks))
 
+
 def create_output_string(quote, stock):
     pl = (quote["latestPrice"] - stock['buy_price']) * stock['shares']
     color = "red" if pl < 0 else "green"
     quote_url = 'https://swingtradebot.com/equities/' + stock['stock']
-    return entryString('{}: ${:0.2f} {:0.2f} [STOP: {:0.2f} / SELL: {:0.2f}]'.format(stock['stock'], quote["latestPrice"], pl, stock['stop'], stock['sell']), color=color, href=quote_url)
+    return entry_string(
+        '{}: ${:0.2f} {:0.2f} [STOP: {:0.2f} / SELL: {:0.2f}]'.format(
+            stock['stock'],
+            quote["latestPrice"],
+            pl,
+            stock['stop'],
+            stock['sell']
+        ),
+        color=color,
+        href=quote_url
+    )
 
 
-def PLNow(stocks):
-    totalPL=0
-    finalOutput = ''
+def pl_totals(stocks):
+    total_pl = 0
+    final_output = ''
 
     if len(stocks) > 0:
         quotes = get_stock_quotes(stocks)
@@ -137,25 +167,28 @@ def PLNow(stocks):
             stock['sell'] = (stock['buy_price'] * 1.05)
 
             if quote['latestPrice'] >= stock['sell']:
-                sellTitle = "SELL: {}".format(stock['stock'])
-                sellMessage = "Sell Price: {:0.2f}".format(stock['sell'])
+                sell_title = "SELL: {}".format(stock['stock'])
+                sell_message = "Sell Price: {:0.2f}".format(stock['sell'])
                 if stock.get('alert', True):
-                    displayNotification(sellMessage, sellTitle, "Pop")
+                    display_notification(sell_message, sell_title, "Pop")
             elif quote['latestPrice'] <= stock['stop']:
-                stopTitle = "STOP LOSS: {}".format(stock['stock'])
-                stopMessage = "Stop Price: {:0.2f}".format(stock['stop'])
+                stop_title = "STOP LOSS: {}".format(stock['stock'])
+                stop_message = "Stop Price: {:0.2f}".format(stock['stop'])
                 if stock.get('alert', True):
-                    displayNotification(stopMessage, stopTitle, "Ping")
+                    display_notification(stop_message, stop_title, "Ping")
 
-            totalPL += (quote['latestPrice'] - stock['buy_price']) * stock['shares']
-            finalOutput += create_output_string(quote, stock) + '\r\n'
+            total_pl += (quote['latestPrice'] - stock['buy_price']) * stock['shares']
+            final_output += create_output_string(quote, stock) + '\r\n'
 
-        print '{} Active Trades Total ${:0.2f} | color={}'.format(':chart_with_downwards_trend:' if totalPL < 0 else ':chart_with_upwards_trend:', totalPL, "red" if totalPL < 0 else "green")
+        print '{} Active Trades Total ${:0.2f} | color={}'.format(
+            ':chart_with_downwards_trend:' if total_pl < 0 else ':chart_with_upwards_trend:', total_pl,
+            "red" if total_pl < 0 else "green")
         print '---'
-        print finalOutput
+        print final_output
 
-if len(sys.argv) == 1:
-    entry('|templateImage=\'%s\'' % icon)
+
+def initialize():
+    entry('|templateImage=\'%s\'' % scales_icon)
     entry('---')
     entry('Add a trade...', bash=__file__, param1='add', terminal='false', refresh='true')
     entry('---')
@@ -165,22 +198,27 @@ if len(sys.argv) == 1:
             stockObjects = []
             for ticker, stock in stocks.iteritems():
                 stockObjects.append(stock)
-            PLNow(stockObjects)
+            pl_totals(stockObjects)
             entry('---')
             entry('Toggle Alerts:')
             for ticker, stock in stocks.iteritems():
-                alert_icon = ":bell:" if stock.get('alert', True) == True else ":no_bell:"
-                entry('{} {}'.format(alert_icon, ticker), bash=__file__, param1='toggleAlert', param2=ticker, terminal='false', refresh='true')
+                alert_icon = ":bell:" if stock.get('alert', True) is True else ":no_bell:"
+                entry('{} {}'.format(alert_icon, ticker), bash=__file__, param1='toggleAlert', param2=ticker,
+                      terminal='false', refresh='true')
             entry('---')
             entry('Remove stocks:')
             for ticker, stock in stocks.iteritems():
-                entry('{}'.format(ticker), bash=__file__, param1='remove', param2=ticker, terminal='false', refresh='true')
-
+                entry('{}'.format(ticker), bash=__file__, param1='remove', param2=ticker, terminal='false',
+                      refresh='true')
     else:
         create_file(stocks_file, '{}')
         create_file(trade_history, '[]')
     entry('---')
     entry('Data provided for free by IEX.')
+
+
+if len(sys.argv) == 1:
+    initialize()
 elif len(sys.argv) == 2 and sys.argv[1] == 'add':
     stock = prompt('Stock symbol')
     price = float(prompt('Price paid per share'))
